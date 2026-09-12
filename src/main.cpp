06f0d48b0192c84414c60d239d1997fcb3ca0bec
Aqtia <aqtianue@gmail.com>
2026-09-12 17:44:41 -0500

    Deleted raygui in project

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define RAYGUI_TEXTBOX_AUTO_CURSOR_DELAY 5
 #define RAYGUI_CUSTOM_ROUNDED_RECTS
 #define RAYGUI_IMPLEMENTATION
-#include "../resources/raygui.h"
+#include "../../Raylib/raygui.h"
 
 #include "../resources/tinyfiledialogs.h"
 #include <fstream>
